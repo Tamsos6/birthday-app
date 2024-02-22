fa8d8b9cb94eaf4b4af5bce4d72908a76e46d33f
Tamsos6 <alan.tam7@hotmail.com>
2024-02-22 23:42:49 +1100

    small commit

--- birthday-app/src/Asian.tsx
+++ birthday-app/src/Asian.tsx
@@ -16,7 +16,6 @@ const Asian: React.FC = () => {
     setRestaurant(selectedRestaurant);
     const combinedState = { selectedRestaurant, date}
     navigate('/final', { state: combinedState });
-    // console.log(combinedState);
   };
   
   return (
@@ -25,29 +24,29 @@ const Asian: React.FC = () => {
             <h1>Asian</h1>
           <div className="button-container">
             <div className="rest-container">
-              <p>Ito Restaurant - Surry Hills</p>
-              <img src={ito} alt="Asian" className="cuisine-image"   onClick={() => window.location.href = "https://cdn.sanity.io/files/q60682qe/production/ff8ba178c0e1937f3cc659d0c2b5a37a5f6fcb96.pdf/ITO%20A%20la%20Carte%20Menu_January2024_online.pdf"} />
+              <h3>Ito Restaurant - Surry Hills</h3>
+              <img src={ito} alt="Asian" className="cuisine-image" style={{ paddingBottom: "30px" }} onClick={() => window.location.href = "https://cdn.sanity.io/files/q60682qe/production/ff8ba178c0e1937f3cc659d0c2b5a37a5f6fcb96.pdf/ITO%20A%20la%20Carte%20Menu_January2024_online.pdf"} />
               <button className="button" onClick={ () => handleRestaurant("Ito Restaurant")} >
-                This option
+                Select Me!
               </button>
             </div>
             <div className="rest-container">
-              <p>Jazzushi - Surry Hills</p>
-              <img src={asian} alt="Asian" className="cuisine-image"/>
+              <h3>Jazzushi - Surry Hills</h3>
+              <img src={asian} alt="Asian" className="cuisine-image" style={{ paddingBottom: "30px" }}/>
               <button className="button" onClick={ () => handleRestaurant("Jazzushi")} >
                 This option
               </button>
             </div>
             <div className="rest-container">
-              <p>Jazzushi - Surry Hills</p>
-              <img src={asian} alt="Asian" className="cuisine-image"/>
+              <h3>Jazzushi - Surry Hills</h3>
+              <img src={asian} alt="Asian" className="cuisine-image" style={{ paddingBottom: "30px" }}/>
               <button className="button" onClick={ () => handleRestaurant("Jazzushi")} >
                 This option
               </button>
             </div>
             <div className="rest-container">
-              <p>Jazzushi - Surry Hills</p>
-              <img src={asian} alt="Asian" className="cuisine-image"/>
+              <h3>Jazzushi - Surry Hills</h3>
+              <img src={asian} alt="Asian" className="cuisine-image" style={{ paddingBottom: "30px" }}/>
               <button className="button" onClick={ () => handleRestaurant("Jazzushi")} >
                 This option
               </button>
